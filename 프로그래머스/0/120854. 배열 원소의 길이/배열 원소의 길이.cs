ac55c5afdc847e1d91fcda0e26ae09bd1e2c525c
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-03-20 16:10:13 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.35 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution {
+    public int[] solution(string[] strlist) {
+        int[] answer= new int[strlist.Length];
+for(int i = 0; i < strlist.Length; i++)
+{
+    answer [i]=strlist[i].ToCharArray().Length;
+
+}
+        return answer;
+    }
+}
