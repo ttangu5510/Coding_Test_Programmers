1ed58d0028b647e31d3ddb8c0dc3132c3db87194
ttangu5510 <57214182+ttangu5510@users.noreply.github.com>
2025-03-20 16:09:46 +0900

    [level 0] Title: 편지, Time: 0.14 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120898. 편지/편지.cs
@@ -0,0 +1,10 @@
+using System;
+
+public class Solution {
+    public int solution(string message) {
+        int answer=0;
+        int sizeMessage= message.Length*2;
+        answer = sizeMessage;
+        return answer;
+    }
+}
