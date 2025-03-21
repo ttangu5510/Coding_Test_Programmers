22d9c050f4ef4e4ccc10cd70055ae872038658cd
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-03-22 01:58:30 +0900

    [level 0] Title: 2차원으로 만들기, Time: 0.27 ms, Memory: 31.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120842. 2차원으로 만들기/2차원으로 만들기.cs
@@ -0,0 +1,16 @@
+using System;
+
+public class Solution {
+    public int[,] solution(int[] num_list, int n) {        
+        int[,] answer = new int[num_list.Length / n, n];
+        int count = 0;
+        for(int i = 0; i < answer.GetLength(0); i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                answer[i, j] = num_list[count++];
+            }
+        }
+        return answer;
+    }
+}
