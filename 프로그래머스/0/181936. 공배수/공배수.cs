71ba63d5644545061117f6493ae596d2be493fd9
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-03-29 18:30:29 +0900

    [level 0] Title: 공배수, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181936. 공배수/공배수.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int number, int n, int m) {
+        if(number % n == 0)
+        {
+            if(number % m ==0)
+            {
+                return 1;
+            }
+        }
+        return 0;
+    }
+}
