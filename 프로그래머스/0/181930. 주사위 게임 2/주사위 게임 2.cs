f93c5a8fe5ce28fc6903fd287321c2057cf02272
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-03-30 23:50:49 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.30 ms, Memory: 31.1 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -7,21 +7,14 @@ public class Solution {
         {
             answer = (3*a*a*a)*(3*a*a)*(3*a);
         }
-        else if (a == b&&a!=c)
-        {
-            answer = (2 * a + c) * (2 * a * a + c * c);
-        }
-        else if (a == c && a != b)
-        {
-            answer = (2 * a + b) * (2 * a * a + b * b);
-        }
-        else if (b == c && b != a)
-        {
-            answer = (2 * b + a) * (2 * b * b + a * a);
-        }
-        else
+        else if (a != b&&b!=c&&a!=c)
         {
             answer = a + b + c;
+        }
+    
+        else
+        {
+            answer = (a + b + c)*(a*a+b*b+c*c);
         }
         return answer;
     }
