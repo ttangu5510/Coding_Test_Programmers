1988b1b4bc6a55caa9b5d8ca92a8f7ae762e5c7d
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-08-10 00:35:51 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 0.82 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int n) {
+        List<int> answer = new List<int>();
+        while(n >1)
+        {
+            answer.Add(n);
+            if(n%2 ==0)
+            {
+                n = n / 2;
+            }
+            else
+            {
+                n = 3 * n + 1;
+            }
+            
+        }
+        answer.Add(1);
+        return answer.ToArray();
+    }
+}
