5ca8561a859e3207b6efaca41ac747f615cc6fb0
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-08-10 00:52:36 +0900

    [level 0] Title: 배열 만들기 4, Time: 3.75 ms, Memory: 32.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        List<int>stk = new List<int>();
+        int i = 0;
+        while (i<arr.Length)
+        {
+            if(stk.Count == 0)
+            {
+                stk.Add(arr[i]);
+                i++;
+            }
+            else if(stk.Count>0 && stk[stk.Count-1]<arr[i])
+            {
+                stk.Add(arr[i]);
+                i++;
+            }
+            else if (stk.Count>0 && stk[stk.Count-1]>=arr[i])
+            {
+                stk.RemoveAt(stk.Count-1);
+            }
+        }
+
+        return stk.ToArray();
+    }
+}
