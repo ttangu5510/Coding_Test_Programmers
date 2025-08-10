73be4468fd178c257aedec0e4b706cf665a01677
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-08-10 19:39:49 +0900

    [level 0] Title: 글자 이어 붙여 문자열 만들기, Time: 2.44 ms, Memory: 34.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public string solution(string my_string, int[] index_list) {
+        string answer = "";
+        for(int i =0;i<index_list.Length;i++)
+        {
+            answer=$"{answer}{my_string[index_list[i]]}";
+        }
+        return answer;
+    }
+}
