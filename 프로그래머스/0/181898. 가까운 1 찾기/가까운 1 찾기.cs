e2510f0c9c3eda880d1e5d2c13a306212f66677e
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-08-16 16:03:05 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.17 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Solution {
+    public int solution(int[] arr, int idx) {
+        int answer = 0;
+        for(int i = idx ; i<arr.Length;i++)
+        {
+            if(arr[i] == 1)
+            {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
