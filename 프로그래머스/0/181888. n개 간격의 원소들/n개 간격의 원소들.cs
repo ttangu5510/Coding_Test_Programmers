3b2a9503e570f8bfb69e43cd029c2b08505e7439
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-08-30 22:09:52 +0900

    [level 0] Title: n개 간격의 원소들, Time: 2.83 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cs
+++ 프로그래머스/0/181888. n개 간격의 원소들/n개 간격의 원소들.cs
@@ -1,17 +1,10 @@
 using System;
+using System.Linq;
 
 public class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[((num_list.Length - 1) / n) + 1];
-        int count = 0;
-        for(int i = 0 ; i < num_list.Length ; i++)
-        {
-            if(i % n  == 0)
-            {
-                answer[count] = num_list[i];
-                count++;
-            }
-        }
-        return answer;
+        return num_list
+            .Where((value,index) => index % n == 0)
+            .ToArray();
     }
 }
