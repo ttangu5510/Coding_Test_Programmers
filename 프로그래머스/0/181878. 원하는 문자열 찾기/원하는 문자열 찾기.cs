45ddce5c04d4fbd714ac57a2f6016a0a2ba85b49
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-08-31 23:24:40 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 2.42 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cs
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.cs
@@ -2,7 +2,6 @@ using System;
 
 public class Solution {
     public int solution(string myString, string pat) {
-        if(myString.ToUpper().Contains(pat.ToUpper())) return 1;
-        return 0;
+        return (myString.ToUpper().Contains(pat.ToUpper()) == true) ? 1 : 0;
     }
 }
