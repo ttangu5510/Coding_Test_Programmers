4d5bb9d2815f3774f8708dde2a10eb0b9abecfda
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-10-16 16:25:03 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 2.71 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.cs
@@ -3,12 +3,6 @@ using System.Collections.Generic;
 
 public class Solution {
     public string[] solution(string my_string) {
-        string[] tmp = my_string.Split(' ');
-        List<string> answer = new List<string>();
-        foreach(string s in tmp)
-        {
-            if(s != "") answer.Add(s);
-        }
-        return answer.ToArray();
+        return my_string.Split(' ',StringSplitOptions.RemoveEmptyEntries);
     }
 }
