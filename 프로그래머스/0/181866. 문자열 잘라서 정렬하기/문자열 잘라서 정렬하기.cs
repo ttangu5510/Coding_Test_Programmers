3a9b15205c939d7989aa6e09695d7d0cfc1f4821
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-11-14 00:47:20 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 151.94 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution {
+    public string[] solution(string myString) {
+        string[] answer = myString.Split('x',StringSplitOptions.RemoveEmptyEntries);
+        Array.Sort(answer);
+        return answer;
+    }
+}
