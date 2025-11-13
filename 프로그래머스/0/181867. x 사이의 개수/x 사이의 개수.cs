bfb73a48fde91f577c1c73cf449b0dfce1e6ab15
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-11-14 00:33:31 +0900

    [level 0] Title: x 사이의 개수, Time: 5.91 ms, Memory: 33.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.cs
@@ -0,0 +1,8 @@
+using System;
+using System.Linq;
+
+public class Solution {
+    public int[] solution(string myString) {
+        return myString.Split('x').Select(x=>x.Length).ToArray();
+    }
+}
