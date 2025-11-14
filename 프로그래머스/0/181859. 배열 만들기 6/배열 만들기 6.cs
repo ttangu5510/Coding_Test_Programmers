f78a88618e0df452eb6fe272d5f1101a95541eb2
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-11-14 18:38:33 +0900

    [level 0] Title: 배열 만들기 6, Time: 33.25 ms, Memory: 49.1 MB -BaekjoonHub

--- 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cs
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cs
@@ -1,25 +1,26 @@
 using System;
+using System.Linq;
 using System.Collections.Generic;
 
 public class Solution {
     public int[] solution(int[] arr) {
-        List<int> answer = new List<int>();
+        Stack<int> answer = new Stack<int>();
         for(int i =  0 ; i < arr.Length ; i++)
         {
             if(answer.Count == 0)
-                answer.Add(arr[i]);
+                answer.Push(arr[i]);
             else
             {
-                if(answer[answer.Count-1] == arr[i])
+                if(answer.Peek() == arr[i])
                 {
-                    answer.RemoveAt(answer.Count-1);
+                    answer.Pop();
                 }
                 else
                 {
-                    answer.Add(arr[i]);
+                    answer.Push(arr[i]);
                 }
             }
         }
-        return answer.Count == 0 ? new int[1]{-1}: answer.ToArray();
+        return answer.Count == 0 ? new int[1]{-1}: answer.Reverse().ToArray();
     }
 }
