fa7cc3e16b4513f1c02e8f669cb6e96d7c7f97e3
ttangu <57214182+ttangu5510@users.noreply.github.com>
2025-11-16 16:42:52 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 4.73 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.cs
@@ -4,12 +4,6 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] num_list) {
-        Array.Sort(num_list);
-        int[] answer = new int[num_list.Length - 5];
-        for(int i = 5 ; i < num_list.Length ; i++)
-        {
-            answer[i-5] = num_list[i];
-        }
-        return answer;
+        return num_list.OrderBy(o=>o).Skip(5).ToArray();
     }
 }
