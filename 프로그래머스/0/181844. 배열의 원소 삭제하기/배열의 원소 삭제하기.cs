7c40878a4f86ada4f2034b2c91585c7152653bf5
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-12 06:29:03 +0900

    [level 0] Title: 배열의 원소 삭제하기, Time: 1.95 ms, Memory: 31.8 MB -BaekjoonHub

--- 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cs
+++ 프로그래머스/0/181844. 배열의 원소 삭제하기/배열의 원소 삭제하기.cs
@@ -4,8 +4,6 @@ using System.Linq;
 
 public class Solution {
     public int[] solution(int[] arr, int[] delete_list) {
-        List<int>answer = new List<int>();
-        answer = arr.Where(x => !delete_list.Contains(x)).ToList();
-        return answer.ToArray();
+        return arr.Where(x => !delete_list.Contains(x)).ToArray();
     }
 }
