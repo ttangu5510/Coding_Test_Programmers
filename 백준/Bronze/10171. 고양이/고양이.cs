9d74dd84edd178eb3dce3d61b4f1354cc1c2baa2
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-12 08:05:40 +0900

    [Bronze V] Title: 고양이, Time: 40 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine($"\\    /\\");
+        Console.WriteLine($" )  ( ')");
+        Console.WriteLine($"(  /  )");
+        Console.WriteLine($" \\(__)|");
+    }
+}
