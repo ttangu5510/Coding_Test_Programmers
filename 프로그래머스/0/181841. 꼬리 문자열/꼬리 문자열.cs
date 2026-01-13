4696c63bcb27a8d204ffd41713f0f381cc8583aa
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-13 15:02:19 +0900

    [level 0] Title: 꼬리 문자열, Time: 1.08 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
+++ 프로그래머스/0/181841. 꼬리 문자열/꼬리 문자열.cs
@@ -1,13 +1,7 @@
 using System;
-using System.Text;
+using System.Linq;
 
 public class Solution {
-    public string solution(string[] str_list, string ex) {
-        StringBuilder answer = new StringBuilder();
-        foreach(var s in str_list)
-        {
-            if(!s.Contains(ex)) answer.Append(s);
-        }
-        return answer.ToString();
-    }
+    public string solution(string[] str_list, string ex)
+        => string.Concat(str_list.Where(s=>!s.Contains(ex)));
 }
