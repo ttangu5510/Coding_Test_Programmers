a0db5f894ab9294fbd88f3b9f1632b45bfa24a33
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-14 17:20:09 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.39 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cs
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cs
@@ -4,12 +4,7 @@ public class Solution {
     public int[,] solution(int n) {
         int[,] answer = new int[n,n];
         for(int i = 0 ; i < n ; i++)
-        {
-            for(int j = 0 ; j < n ; j++)
-            {
-                if(i == j) answer[i,j] = 1;
-            }
-        }
+            answer[i,i] = 1;
         return answer;
     }
 }
