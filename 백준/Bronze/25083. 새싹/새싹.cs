c664bb53996bf961e91ac91ec0666aa50ac1b08f
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-14 17:50:11 +0900

    [Bronze V] Title: 새싹, Time: 44 ms, Memory: 5316 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("         ,r'\"7");
+        Console.WriteLine("r`-_   ,'  ,/");
+        Console.WriteLine(" \\. \". L_r'");
+        Console.WriteLine("   `~\\/");
+        Console.WriteLine("      |");
+        Console.WriteLine("      |");
+    }
+}
