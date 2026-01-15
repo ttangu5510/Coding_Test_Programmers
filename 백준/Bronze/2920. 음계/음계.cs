4bf85ccce068e31f58629aac658002083dbb8ea8
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-16 00:12:05 +0900

    [Bronze II] Title: 음계, Time: 48 ms, Memory: 5488 KB -BaekjoonHub

---
+++ 백준/Bronze/2920. 음계/음계.cs
@@ -0,0 +1,37 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		var input = Console.ReadLine();
+		if(string.IsNullOrEmpty(input)) return;
+			
+		int[] nums = Array.ConvertAll(input.Split(' ', StringSplitOptions.RemoveEmptyEntries), int.Parse);
+		bool ascending = nums[0] == 1 && nums[1] == 2;
+		bool descending = nums[0] == 8 && nums[1] == 7;
+		string result = "";
+		
+		for(int i = 2 ; i < nums.Length - 1 ; i++)
+		{
+			if(!ascending && !descending) break;
+			
+			if(ascending && nums[i] >= nums[i+1])
+			{
+				ascending = false;
+				break;
+			}
+			else if(descending && nums[i] <= nums[i+1])
+			{
+				descending = false;
+				break;
+			}
+		}
+		
+		if(ascending) result = "ascending";
+		else if(descending) result = "descending";
+		else result = "mixed";
+		
+		Console.WriteLine(result);
+	}
+}
