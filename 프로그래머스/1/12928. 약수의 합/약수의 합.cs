f1b3ef7332d1b58d0fe51ea67abfa1c9499fbba5
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-19 15:49:09 +0900

    [level 1] Title: 약수의 합, Time: 0.18 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/1/12928. 약수의 합/약수의 합.cs
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.cs
@@ -2,9 +2,9 @@ using System.Collections.Generic;
 
 public class Solution {
     public int solution(int n) {
-        int answer = 0;
-        answer += n;
-        for(int i = 1; i * 2 <= n ; i++)
+        int answer = n;
+        int halfNum = n / 2;
+        for(int i = 1; i <= halfNum ; i++)
         {
             if(n % i == 0) answer += i;
         }
