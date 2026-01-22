16ea54313213614b18198dafbfbfac9496b59f61
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-22 17:34:19 +0900

    [Silver V] Title: 영화감독 숌, Time: 152 ms, Memory: 18372 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cs
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cs
@@ -2,20 +2,22 @@ using System;
 
 public class Program
 {
-	public static List<int> movieNum = new List<int>();
 	public static void Main()
 	{
 		var input = Console.ReadLine();
 		if(string.IsNullOrEmpty(input)) return;
 		
 		int num = int.Parse(input);
+        int count = 0;
+        int answer = 0;
 		
-		for(int i = 666 ; movieNum.Count <= num ; i++)
+		for(int i = 666 ; count < num ; i++)
 		{
 			string s = i.ToString();
-			if(s.Contains("666")) movieNum.Add(i);
+			if(s.Contains("666")) count++;
+            if(count == num) answer = i;
 		}
 		
-		Console.WriteLine(movieNum[num - 1]);
+		Console.WriteLine(answer);
 	}
 }
