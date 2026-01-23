3de097a16a024471f364dcd85894bd4ae98927fd
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-23 22:07:20 +0900

    [level 1] Title: 가운데 글자 가져오기, Time: 0.18 ms, Memory: 31 MB -BaekjoonHub

--- 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
+++ 프로그래머스/1/12903. 가운데 글자 가져오기/가운데 글자 가져오기.cs
@@ -1,4 +1,4 @@
 public class Solution {
     public string solution(string s)
-        => s.Substring(s.Length % 2 == 0 ? s.Length / 2 -1 : s.Length / 2, s.Length % 2== 0? 2: 1);
+        => s.Substring((s.Length - 1) / 2, (s.Length - 1) % 2 + 1);
 }
