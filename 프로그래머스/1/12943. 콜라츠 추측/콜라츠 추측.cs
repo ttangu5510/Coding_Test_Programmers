e2239dc1125e16f55947e237b92141e691ebe3bd
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-23 21:10:53 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.25 ms, Memory: 31.4 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.cs
@@ -9,8 +9,6 @@ public class Solution {
             
             if (input % 2 == 0) input /= 2;
             else input = input * 3 + 1;
-            
-            Console.WriteLine(input);
         }
         return -1;
     }
