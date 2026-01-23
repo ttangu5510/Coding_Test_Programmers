04276181fe2bb1331382934ebb09ef64fe9329a5
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-23 21:03:03 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.56 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cs
@@ -2,5 +2,5 @@ using System;
 
 public class Solution {
     public string solution(string[] seoul) 
-        => $"김서방은 {Array.IndexOf(seoul, "Kim")}에 있다";
+        => $"김서방은 {Array.FindIndex(seoul, i => i=="Kim")}에 있다";
 }
