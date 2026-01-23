e47e22589d188f21b2c2d205307bfd3768dc4359
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-23 21:55:22 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.22 ms, Memory: 30.8 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cs
@@ -1,14 +1,5 @@
-using System.Linq;
-using System.Text;
-
 public class Solution {
     public string solution(string phone_number) {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < phone_number.Length; i++)
-        {
-            if(i < phone_number.Length - 4) sb.Append('*');
-            else sb.Append(phone_number[i]);
-        }
-        return sb.ToString();
+        return phone_number.Substring(phone_number.Length - 4).PadLeft(phone_number.Length, '*');
     }
 }
