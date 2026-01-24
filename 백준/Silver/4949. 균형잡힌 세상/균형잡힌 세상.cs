387b9e475cc94fa6e50b70a4dfe9b8e5720fc6e1
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-25 02:07:50 +0900

    [Silver IV] Title: 균형잡힌 세상, Time: 56 ms, Memory: 6328 KB -BaekjoonHub

--- 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cs
+++ 백준/Silver/4949. 균형잡힌 세상/균형잡힌 세상.cs
@@ -1,18 +1,26 @@
 using System;
+using System.IO;
+using System.Text;
 using System.Collections.Generic;
 
 public class Program
 {
 	public static void Main()
 	{
+		StreamReader sr = new StreamReader(new BufferedStream(Console.OpenStandardInput()));
+		StreamWriter sw = new StreamWriter(new BufferedStream(Console.OpenStandardOutput()));
+		
+        StringBuilder sb = new StringBuilder();
+		Stack<char> br = new Stack<char>();
+		bool hasBr;
+		
 		while(true)
 		{
-			var input = Console.ReadLine();
-			if(string.IsNullOrEmpty(input) || input == ".") return;
+			var input = sr.ReadLine();
+			if(string.IsNullOrEmpty(input) || input == ".") break;
 			
-			Stack<char> br = new Stack<char>();
-			
-			bool hasBr = true;
+			br.Clear();
+			hasBr = true;
 			
 			for(int i = 0; i < input.Length; i++)
 			{
@@ -38,10 +46,17 @@ public class Program
 						}
 						break;
 				}
+				
 				if(!hasBr) break;
 			}
-			if(hasBr && br.Count == 0) Console.WriteLine("yes");
-            else Console.WriteLine("no");
+			
+			if(hasBr && br.Count == 0) sb.AppendLine("yes");
+			else sb.AppendLine("no");
 		}
+		
+        sw.Write(sb);
+        
+        sr.Close();
+        sw.Close();
 	}
 }
