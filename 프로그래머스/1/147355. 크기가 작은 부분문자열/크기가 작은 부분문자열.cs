a29fd8fc2771a452336d95b35f4017b30971d878
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-28 01:11:57 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 7.28 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
@@ -1,17 +1,9 @@
 using System;
+using System.Linq;
 
 public class Solution {
-    public int solution(string t, string p) {
-        int answer = 0;
-        long pNum = long.Parse(p);
-        
-        for(int i = 0; i <t.Length - p.Length + 1;i++)
-        {
-            string s = t.Substring(i,p.Length);
-            long num = long.Parse(s);
-            if(num <= pNum) answer++;
-        }
-        
-        return answer;
-    }
+    public int solution(string t, string p) =>
+    Enumerable.Range(0, t.Length - p.Length + 1)
+              .Count(i => string.Compare(t, i, p, 0, p.Length) <= 0);
+
 }
