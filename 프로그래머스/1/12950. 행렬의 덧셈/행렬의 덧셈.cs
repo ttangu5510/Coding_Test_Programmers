4fe07cc0a2d4c08b1b292f61b17f2194931692ac
ttangu <57214182+ttangu5510@users.noreply.github.com>
2026-01-27 23:58:24 +0900

    [level 1] Title: 행렬의 덧셈, Time: 2.00 ms, Memory: 52.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2) {
+        int x = arr1.GetLength(1);
+        int y = arr1.GetLength(0);
+        int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
+        
+        for(int i = 0; i < y ; i++)
+        {
+            for(int j = 0; j < x; j++)
+            {
+                answer[i, j] = arr1[i, j] + arr2[i, j];
+            }
+        }
+        
+        return answer;
+    }
+}
